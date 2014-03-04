4683298ece112677142c38e4c3bca454dfdbf340
ppj <prasanna_3003@yahoo.com>
2014-03-05 00:16:16 +1100

    Added capability to handle spares

--- bowling.rb
+++ bowling.rb
@@ -1,34 +1,56 @@
 class Bowling
-  attr_reader :score, :frame_score
+  attr_accessor :roll_scores, :frame_scores
 
   def initialize
-    @score = 0
-    @roll_counter = 0
-    @frame_score = 0
-    @extra_roll = false
+    @roll_scores = []
+    @frame_scores = []
   end
 
   def roll(pins)
-    if pins > 10
+    if 0 > pins or pins > 10
       raise "invalid roll-score"
     end
-    @roll_counter+=1
-    if @roll_counter > 21
+    if @roll_scores.length > 20
       raise "game is definitely over"
     end
-    @frame_score == 10 ? @score += pins :
-    if @roll_counter == 21 and @frame_score < 10
-      raise "game is over"
+    if @roll_scores.length == 20
+      if @roll_scores[18]+@roll_scores[19] < 10
+        raise "game is over"
+      end
     end
-    if @roll_counter%2 == 0 and @roll_counter < 21
-      @frame_score+=pins
-    else
-      @frame_score=pins
-    end
-    if @roll_counter < 21
-      @score += pins
-    end
+    @roll_scores.push(pins)
+  end
 
+  def score
+    frame_roll_index = 1
+    spare_flag = false
+    strike_flag = 0
+    @roll_scores.each do |roll_score|
+      if spare_flag
+        @frame_scores[-1] += roll_score
+        spare_flag = false
+      end
+      if strike_flag > 0
+        @frame_scores[strike_flag-3] += roll_score
+        strike_flag -= 1 if roll_score < 10
+      end
+      if frame_roll_index == 1
+        @frame_scores.push roll_score
+        if @frame_scores.length < 10 and roll_score == 10
+          strike_flag == 0 ? strike_flag = 2 : strike_flag += 1
+        else
+          frame_roll_index = 2
+        end
+        #roll_score != 10 ? frame_roll_index = 2: strike_flag = 2
+      else
+        @frame_scores[-1] += roll_score
+        if @frame_scores.length < 10
+          frame_roll_index = 1
+          spare_flag = @frame_scores[-1]==10
+        end
+      end
+    end
+    @frame_scores.inject(0) { |total_score, frame_score| total_score + frame_score }
   end
 
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -3,22 +3,40 @@ require_relative '../bowling.rb'
 
 describe Bowling do
 
+  def hit_spare firstRoll
+    subject.roll(firstRoll)
+    subject.roll(10-firstRoll)
+  end
+
   context "A single player" do
 
-    it "cannot hit more than 10 pins in a roll" do
+    it "cannot hit less than 0 or more than 10 pins in a roll" do
       expect {subject.roll(12)}.to raise_error("invalid roll-score")
-    end
-
-    context "when hits no spares or strikes" do
-      it "can roll exactly twice in each of the 10 frames" do
-        20.times {subject.roll(1)}
-        expect{subject.roll(10)}.to raise_error("game is over")
-      end
     end
 
     it "cannot roll more than 21 times" do
       21.times {subject.roll(5)}
       expect{subject.roll(3)}.to raise_error("game is definitely over")
+    end
+
+    it "can see the roll scores stored" do
+      rolls=[4,3,6,4,3,2,4,3,2,4,3,2,4,3,2,4,3,2,1,5]
+      rolls.each {|pins| subject.roll(pins)}
+      subject.roll_scores.should == rolls
+    end
+
+    it "can see the frame scores stored" do
+      rolls=[ 7,3, 4,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0 ]
+      rolls.each {|pins| subject.roll(pins)}
+      subject.score
+      subject.frame_scores.should == [14,4,0,0,0,0,0,0,0,0]
+    end
+
+    context "when hits no spares or strikes" do
+      it "can roll exactly twice in each of the 10 frames" do
+        20.times {subject.roll(1)}
+        expect{subject.roll(3)}.to raise_error("game is over")
+      end
     end
 
     context "when rolls 20 gutter balls" do
@@ -48,16 +66,18 @@ describe Bowling do
     context "when hits a spare" do
       it "scores double the next roll" do
         16.times {subject.roll(1)}
-        2.times {subject.roll(5)}
+        hit_spare 5
         subject.roll(4)
         subject.roll(1)
-        subject.score.should eql (35)
+        subject.score.should == (35)
       end
     end
 
     context "when hits 5 pins in each roll (all spares)" do
       it "scores 150" do
-        21.times {subject.roll(5)}
+        #21.times {subject.roll(5)}
+        10.times {hit_spare 5}
+        subject.roll(5)
         subject.score.should eql(150)
       end
     end
@@ -65,13 +85,39 @@ describe Bowling do
     context "when hits 7+3 pins respectively in each frame (all spares)" do
       it "scores 170" do
         10.times {
-          subject.roll(7)
-          subject.roll(3)
+          #subject.roll(7)
+          #subject.roll(3)
+          hit_spare(7)
         }
         subject.roll(7)
         subject.score.should eql(170)
       end
     end
+
+    context "when hits one or many (non-consecutive) strikes" do
+      it "gets next two rolls as bonus points for the strike frame" do
+        subject.roll(10)
+        subject.roll(3)
+        subject.roll(4)
+        subject.roll(10)
+        subject.roll(1)
+        subject.roll(2)
+        14.times {subject.roll(0)}
+        subject.score
+        subject.frame_scores.should == [17,7,13,3,0,0,0,0,0,0]
+      end
+    end
+
+    #context "when hits consecutive strikes in the first 2 frames" do
+    #  it "gets next two rolls as bonus points for the strike frame" do
+    #    2.times {subject.roll(10)}
+    #    subject.roll(1)
+    #    subject.roll(4)
+    #    14.times {subject.roll(0)}
+    #    subject.score
+    #    subject.frame_scores.should == [21,15,5,0,0,0,0,0,0,0]
+    #  end
+    #end
 
   end
 
