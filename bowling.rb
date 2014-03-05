b80b963477b2ddfc0f879bb25c2de5f0d6e6932c
ppj <prasanna_3003@yahoo.com>
2014-03-05 11:34:13 +1100

    Strike tests and some major refactoring

--- bowling.rb
+++ bowling.rb
@@ -23,30 +23,21 @@ class Bowling
 
   def score
     frame_roll_index = 1
-    spare_flag = false
-    strike_flag = 0
-    @roll_scores.each do |roll_score|
-      if spare_flag
-        @frame_scores[-1] += roll_score
-        spare_flag = false
-      end
-      if strike_flag > 0
-        @frame_scores[strike_flag-3] += roll_score
-        strike_flag -= 1 if roll_score < 10
-      end
+    @roll_scores.each_with_index do |roll_score, roll_index|
       if frame_roll_index == 1
         @frame_scores.push roll_score
-        if @frame_scores.length < 10 and roll_score == 10
-          strike_flag == 0 ? strike_flag = 2 : strike_flag += 1
+        if @frame_scores.length < 10 and roll_score == 10 # strike
+          frame_scores[-1] += roll_scores[roll_index+1]+roll_scores[roll_index+2]
         else
           frame_roll_index = 2
         end
-        #roll_score != 10 ? frame_roll_index = 2: strike_flag = 2
       else
         @frame_scores[-1] += roll_score
         if @frame_scores.length < 10
           frame_roll_index = 1
-          spare_flag = @frame_scores[-1]==10
+          if @frame_scores[-1]==10  # spare
+            frame_scores[-1] += roll_scores[roll_index+1]
+          end
         end
       end
     end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -8,6 +8,14 @@ describe Bowling do
     subject.roll(10-firstRoll)
   end
 
+  def hit_strike
+    subject.roll(10)
+  end
+
+  def gutter_roll
+    subject.roll(0)
+  end
+
   context "A single player" do
 
     it "cannot hit less than 0 or more than 10 pins in a roll" do
@@ -15,7 +23,9 @@ describe Bowling do
     end
 
     it "cannot roll more than 21 times" do
-      21.times {subject.roll(5)}
+      18.times {subject.roll(4)}
+      hit_spare(7)
+      subject.roll(8)
       expect{subject.roll(3)}.to raise_error("game is definitely over")
     end
 
@@ -39,16 +49,16 @@ describe Bowling do
       end
     end
 
-    context "when rolls 20 gutter balls" do
+    context "when plays the worst game" do
       it "scores 0" do
-        20.times {subject.roll(0)}
+        20.times {gutter_roll}
         subject.score.should eql(0)
       end
     end
 
-    context "when hits 1 pin in two consecutive rolls" do
+    context "when hits a single pin in two consecutive rolls" do
       it "scores 2" do
-        18.times { subject.roll(0) }
+        18.times { gutter_roll }
         2.times  { subject.roll(1) }
         subject.score.should eql(2)
       end
@@ -56,15 +66,15 @@ describe Bowling do
 
     context "when hits total 10 pins in consecutive rolls in 2 different frames" do
       it "scores 10" do
-        subject.roll(0)
+        gutter_roll
         2.times  { subject.roll(5) }
-        17.times { subject.roll(0) }
-        subject.score.should eql(10)
+        17.times { subject.roll(1) }
+        subject.score.should eql(27)
       end
     end
 
     context "when hits a spare" do
-      it "scores double the next roll" do
+      it "scores double the next roll (spare bonus)" do
         16.times {subject.roll(1)}
         hit_spare 5
         subject.roll(4)
@@ -75,7 +85,6 @@ describe Bowling do
 
     context "when hits 5 pins in each roll (all spares)" do
       it "scores 150" do
-        #21.times {subject.roll(5)}
         10.times {hit_spare 5}
         subject.roll(5)
         subject.score.should eql(150)
@@ -85,8 +94,6 @@ describe Bowling do
     context "when hits 7+3 pins respectively in each frame (all spares)" do
       it "scores 170" do
         10.times {
-          #subject.roll(7)
-          #subject.roll(3)
           hit_spare(7)
         }
         subject.roll(7)
@@ -96,28 +103,85 @@ describe Bowling do
 
     context "when hits one or many (non-consecutive) strikes" do
       it "gets next two rolls as bonus points for the strike frame" do
-        subject.roll(10)
+        hit_strike
         subject.roll(3)
         subject.roll(4)
-        subject.roll(10)
+        hit_strike
         subject.roll(1)
         subject.roll(2)
-        14.times {subject.roll(0)}
+        14.times {gutter_roll}
         subject.score
         subject.frame_scores.should == [17,7,13,3,0,0,0,0,0,0]
       end
     end
 
-    #context "when hits consecutive strikes in the first 2 frames" do
-    #  it "gets next two rolls as bonus points for the strike frame" do
-    #    2.times {subject.roll(10)}
-    #    subject.roll(1)
-    #    subject.roll(4)
-    #    14.times {subject.roll(0)}
-    #    subject.score
-    #    subject.frame_scores.should == [21,15,5,0,0,0,0,0,0,0]
-    #  end
-    #end
+    context "when hits consecutive strikes in the first 3 frames" do
+      it "gets next two rolls as bonus points for every strike frame" do
+        3.times {subject.roll(10)}
+        subject.roll(2)
+        subject.roll(3)
+        12.times {gutter_roll}
+        subject.score
+        subject.frame_scores.should == [30,22,15,5,0,0,0,0,0,0]
+      end
+    end
+
+    context "when plays the best game" do
+      it "scores 300" do
+        12.times {subject.roll(10)}
+        subject.score.should == 300
+      end
+    end
+
+    context "when hits strike and then a spare in the following frame" do
+      it "gets the strike and spare bonuses correctly" do
+        2.times {subject.roll(3)}
+        subject.roll(10)
+        subject.roll(6)
+        subject.roll(4)
+        subject.roll(5)
+        13.times {gutter_roll}
+        subject.score
+        subject.frame_scores.should == [6,20,15,5,0,0,0,0,0,0]
+      end
+    end
+
+    context "when hits spare and then a strike in the following frame" do
+      it "gets the strike and spare bonuses correctly" do
+        2.times {subject.roll(4)}
+        subject.roll(7)
+        subject.roll(3)
+        subject.roll(10)
+        subject.roll(2)
+        subject.roll(7)
+        12.times {gutter_roll}
+        subject.score
+        subject.frame_scores.should == [8,20,19,9,0,0,0,0,0,0]
+      end
+    end
+
+    context "when hits strikes in the first two rolls of the last frame" do
+      it "gets the extra roll but no bonuses" do
+        18.times {subject.roll(1)}
+        2.times {hit_strike}
+        subject.roll(3)
+        subject.score
+        subject.frame_scores.should == [2,2,2,2,2,2,2,2,2,23]
+      end
+    end
+
+    context "when hits a spare in the first two rolls of the last frame" do
+      it "gets the extra roll but no bonuses" do
+        9.times {
+          subject.roll(1)
+          subject.roll(2)
+        }
+        hit_spare(4)
+        subject.roll(7)
+        subject.score
+        subject.frame_scores.should == [3,3,3,3,3,3,3,3,3,17]
+      end
+    end
 
   end
 
