14caa4317bb36475da93a3b561b287a262d887c7
ppj <prasanna_3003@yahoo.com>
2014-03-05 23:37:22 +1100

    refactored is_strike? and is_spare? methods

--- bowling.rb
+++ bowling.rb
@@ -21,16 +21,16 @@ class Bowling
     @roll_scores.push(pins)
   end
 
-  def is_strike?
-    @frame_scores[-1] == 10
+  def is_strike? roll_index
+    @roll_scores[roll_index]==10
   end
 
   def strike_bonus roll_index
     @roll_scores[roll_index+1]+@roll_scores[roll_index+2]
   end
 
-  def is_spare?
-    @frame_scores[-1] == 10
+  def is_spare? roll_index
+    @roll_scores[roll_index] + @roll_scores[roll_index+1] == 10
   end
 
   def spare_bonus roll_index
@@ -45,16 +45,16 @@ class Bowling
   end
 
   def calculate_frame_scores
-    roll_index  = 0
+    roll_index = 0
     10.times do
       frame_scores.push roll_scores[roll_index]
       if @frame_scores.length < 10
-        if is_strike?
+        if is_strike? roll_index
           @frame_scores[-1] += strike_bonus(roll_index)
           roll_index += 1
         else
           frame_scores[-1] += roll_scores[roll_index+1]
-          if is_spare?
+          if is_spare? roll_index
               @frame_scores[-1] += spare_bonus(roll_index)
           end
           roll_index += 2
